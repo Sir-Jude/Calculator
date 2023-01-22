82024e58fed1a429b490ebd745ca40634663670f
SerJude <giulio.arpaia@gmail.com>
2023-01-22 18:56:40 +0100

    Commit calculator with argv, but there is the issue with multiplication symbol

--- calculator_argv.py
+++ calculator_argv.py
@@ -1,54 +1,34 @@
+# Import "sys" for being able to use "argv" and "exit"
 import sys
+# Import pyjoke for providing the random joke
 import pyjokes
 
-off = "O"
-print("Hello! Please, choose an option: ")
-func = input(
-    'type "J" to hear a joke, "C" to use the calculator or "O" to switch it Off: '
-).upper()
-if func in off:
-    sys.exit("Goodbye!")
-elif func in ["J", "C", "O"]:
-    while True:
-        while func == "J":
-            print(pyjokes.get_joke("en"))
-            print()
-
-            func = input(
-                '2) type "J" to hear a joke, "C" to use the calculator or "O" to switch it Off: '
-            ).upper()
-            if func == "C":
-                continue
-            elif func in off:
-                sys.exit("Goodbye!")
-
-        while func == "C":
-            x, operation, y = input(
-                'type a first number, an operator ( +  -  *  / ), a second number and finally press "Enter": '
-            )
-            if operation == "+":
-                print(float(x) + float(y))
-            elif operation == "-":
-                print(float(x) - float(y))
-            elif operation == "*":
-                print(float(x) * float(y))
-            elif operation == "/":
-                try:
-                    print(float(x) / float(y))
-                except ZeroDivisionError:
-                    print("It is not possible dividing a number for zero.")
-            else:
-                print("Please, type only numbers and an operation without any space:")
-            print()
-
-            func = input(
-                '3) type "J" to hear a joke, "C" to use the calculator or "O" to switch it Off: '
-            ).upper()
-            if func == "J":
-                continue
-            elif func in off:
-                sys.exit("Goodbye!")
+# Exit if user provides a python file and the right number of arguments 
+if sys.argv[0][-3:] != ".py":
+    sys.exit("Not a Python file")
+elif len(sys.argv) > 4: # Too many
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) < 4: # Too few
+    sys.exit("Too few command-line arguments")
 else:
-    sys.exit(
-        'Your option is not valid: please, next time choose only among "J" "C" or "O".'
-    )
+    # Otherwise try to perform the operation
+    try:
+        if sys.argv[2] == "+":
+            print(int(sys.argv[1]) + int(sys.argv[3]))
+        elif sys.argv[2] == "-":
+            print(int(sys.argv[1]) - int(sys.argv[3]))
+        elif sys.argv[2] == "*":
+            print(int(sys.argv[1]) * int(sys.argv[3]))
+        elif sys.argv[2] == "/":
+            try:
+                print(int(sys.argv[1]) / int(sys.argv[3]))
+            # Except the error and exit if User try to perform a zero division
+            except ZeroDivisionError:
+                sys.exit("It is not possible dividing a number for zero.")
+    # Except and exit if User do not use numbers and and a mathematical operator
+    except ValueError:
+        sys.exit("Only numbers and one of the 4 basic mathematical operators (+ - * /) are allowed.")
+
+# Print a random joke
+print()
+print(pyjokes.get_joke("en"))
